0d99c72cbbbeecfd7f6d27d7e19c7ba1b8dfe326
edgardegantea <edgar.degante.a@gmail.com>
2021-04-20 20:17:23 -0500

    Proyecto de ML v1

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Informacion)
+admin.site.register(Temperatura)
+admin.site.register(Area)
---
+++ core/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+# Create your models here.
+class Informacion(models.Model):
+    privacidad = models.TextField(verbose_name='Política de privacidad', max_length=5000)
+    nombre = models.CharField(verbose_name='Nombre de la aplicación', max_length=100)
+    nombreContro = models.CharField(verbose_name='Nombre corto', max_length=20)
+    telefono = models.CharField(verbose_name='Contacto', max_length=100)
+
+    def __str__(self):
+        return self.nombreContro
+
+    class Meta:
+        verbose_name='Información'
+        verbose_name_plural='Datos de la aplicación'
+
+
+# Temperaturas
+class Temperatura(models.Model):
+    temperaturaMinima = models.DecimalField(verbose_name='Temperatura mínima', max_digits=2, decimal_places=2,
+                                            default=0.00)
+    temperaturaMaxima = models.DecimalField(verbose_name='Temperatura máxima', max_digits=2, decimal_places=2, default=0.00)
+    temperaturaLeida = models.DecimalField(verbose_name='Temperatura leída', max_digits=2, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.temperaturaLeida
+
+    class Meta:
+        verbose_name='Temperatuta'
+        verbose_name_plural='Temperaturas'
+
+
+class Area(models.Model):
+    nombre = models.CharField(verbose_name='Lugar', max_length=200)
+    latitud = models.CharField(verbose_name='Latitud', max_length=12)
+    longitud = models.CharField(verbose_name='Longitud', max_length=12)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name='Ubicación'
+        verbose_name_plural='Datos de la ubicación'
+
